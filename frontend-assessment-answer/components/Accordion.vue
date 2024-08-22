f24955586fa9e7837d24fcac9ac5cb9e6511e07c
rezki bimo <rezkibimo@users.noreply.github.com>
2024-08-22 21:29:42 +1000

    update accordion style
    added rotatable chevron

--- frontend-assessment-answer/components/Accordion.vue
+++ frontend-assessment-answer/components/Accordion.vue
@@ -5,11 +5,31 @@
       <button
         class="flex items-center justify-between w-full p-5 font-medium rtl:text-right border border-b-0 border-gray-200 focus:ring-4 focus:ring-blue-200 gap-3"
         :class="{
-          'bg-blue-100 border-blue-500 text-blue-600 font-bold': activeIndex === index,
+          'bg-blue-100 border-blue-500 text-blue-600 font-bold':
+            activeIndex === index,
         }"
         @click="toggle(index)"
       >
         {{ item.title }}
+        <svg
+          data-accordion-icon
+          class="w-3 h-3 shrink-0"
+          :class="{
+            'rotate-180': activeIndex === index,
+          }"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5 5 1 1 5"
+          />
+        </svg>
       </button>
 
       <div :id="index" v-show="activeIndex === index">
