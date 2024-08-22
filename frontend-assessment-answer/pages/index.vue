030e476a113bf5788ddc8e9dbfa75c97b00871fc
rezki bimo <rezkibimo@users.noreply.github.com>
2024-08-22 16:36:47 +1000

    create index.vue
    instead of using app.vue now use page router to support more page

---
+++ frontend-assessment-answer/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <HeroImage />
+    <!-- Cards Grid -->
+    <div class="flex flex-wrap gap-10 justify-center px-6 md:px-0">
+      <ImageCard
+        v-for="(card, index) in cards"
+        :key="index"
+        :cardText="card.text"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const cards = [
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+  },
+];
+</script>
