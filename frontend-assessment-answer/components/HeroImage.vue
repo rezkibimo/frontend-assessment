0e7bba3343c86ae21dfe94b04b5c2dd2241964eb
rezki bimo <rezkibimo@users.noreply.github.com>
2024-08-22 17:08:46 +1000

    hero image
    now responsive

--- frontend-assessment-answer/components/HeroImage.vue
+++ frontend-assessment-answer/components/HeroImage.vue
@@ -1,12 +1,11 @@
 <template>
   <div
-    class="relative h-[600px] md:h-[650px] bg-cover bg-center mb-10"
-    style="background-image: url('https://via.placeholder.com/1920x650')"
+    class="relative h-[600px] md:h-[650px] bg-cover bg-center mb-10 background-image"
   >
     <!-- Text Content -->
     <div class="relative z-10 flex items-center justify-center h-full">
       <div class="text-center text-white px-4">
-        <h1 class="text-5xl	md:text-6xl font-bold">Hello Developer</h1>
+        <h1 class="text-5xl md:text-6xl font-bold">Hello Developer</h1>
         <p class="text-2xl md:text-3xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit
         </p>
@@ -15,9 +14,14 @@
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.background-image {
+  background-image: url("https://via.placeholder.com/600x600");
+}
 
-.test{
-    background-color: black;
+@media (min-width: 640px) {
+  .background-image {
+    background-image: url("https://via.placeholder.com/1920x650");
+  }
 }
 </style>
