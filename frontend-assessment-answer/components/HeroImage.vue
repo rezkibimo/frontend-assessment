94f958f4130a99dec2d7044bb94e170a4c1b04be
rezki bimo <rezkibimo@users.noreply.github.com>
2024-08-22 17:04:39 +1000

    hero image design update
    update to match the required design

--- frontend-assessment-answer/components/HeroImage.vue
+++ frontend-assessment-answer/components/HeroImage.vue
@@ -1,7 +1,16 @@
 <template>
-  <img
-    class="h-auto max-w-full pb-10"
-    src="https://via.placeholder.com/1920x650"
-    alt="image description"
-  />
+  <div
+    class="relative h-[600px] md:h-[650px] bg-cover bg-center mb-10"
+    style="background-image: url('https://via.placeholder.com/1920x650')"
+  >
+    <!-- Text Content -->
+    <div class="relative z-10 flex items-center justify-center h-full">
+      <div class="text-center text-white px-4">
+        <h1 class="text-5xl	md:text-6xl font-bold">Hello Developer</h1>
+        <p class="text-2xl md:text-3xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
